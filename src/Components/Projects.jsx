91dd282bc56bdee4b025ed2480c9a3e363692c88
lucas pereira da silva <lpdslpds595@gmail.com>
2024-05-10 06:56:10 -0300

    fix project name

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -30,18 +30,18 @@ const Projects = () => {
               </div>
             </div>
             <hr className="my-3" />
-            <div className="flex flex-col sm:flex-row gap-3 items-center pb-4">
+            <div className="flex flex-col sm:flex-row gap-6 items-center pb-4">
+              <img src={ rescaldaniwebpage } alt="" className="w-96 rounded-lg shadow-md" />
+              <div>
+              <div>Rescaldani Producciones es una empresa de eventos ubicada en leones. Requerian una web para enseñar un poco de su trabajo así que la desarrolle con React, TailwindCSS, JavaScript, entre otras tecnologías</div>
+                <a href="https://rescaldani-producciones.vercel.app/" className="flex gap-2 text-sky-600 hover:text-sky-800">Rescaldani Producciones<FaExternalLinkAlt /></a>
+              </div>
+            </div>
+            <div className="flex flex-col sm:flex-row gap-6 items-center">
               <img src={ noticierovm } alt="" className="w-96 rounded-lg shadow-md" />
               <div>
               <div>Proyecto realizado con Django poniendo en práctica mis habilidades para el desarrollo back-end. Sistema de busqueda y gestión de publicaciones.</div>
                 <a href="https://noticierovm.fly.dev/" className="flex gap-2 text-sky-600 hover:text-sky-800">NoticieroVM<FaExternalLinkAlt /></a>
-              </div>
-            </div>
-            <div className="flex flex-col sm:flex-row gap-3 items-center">
-              <img src={ rescaldaniwebpage } alt="" className="w-96 rounded-lg shadow-md" />
-              <div>
-              <div>Rescaldani Producciones es una empresa de eventos ubicada en San Marcos. Requerian una web para enseñar un poco de su trabajo así que la desarrolle con React, TailwindCSS, JavaScript, entre otras tecnologías</div>
-                <a href="https://rescaldani-producciones.vercel.app/" className="flex gap-2 text-sky-600 hover:text-sky-800">NoticieroVM<FaExternalLinkAlt /></a>
               </div>
             </div>
           </div>
