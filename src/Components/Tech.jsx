c6c157a9e0578faee82afb69d7597a9024442e78
lucas pereira da silva <lpdslpds595@gmail.com>
2024-05-11 13:17:19 -0300

    tecnologias

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -1,7 +1,8 @@
 import { VscVscode } from "react-icons/vsc";
 import { FaTimes, FaExternalLinkAlt } from "react-icons/fa";
 import { useState } from 'react';
-import { FaPython, FaHtml5, FaCss3, FaJava, FaJs, FaReact, FaNodeJs, FaDocker, FaFigma, FaWordpress  } from "react-icons/fa";
+import { FaPython, FaHtml5, FaCss3, FaJava, FaJs, FaReact, FaDocker, FaFigma, FaWordpress, } from "react-icons/fa";
+import { SiDjango } from "react-icons/si";
 import { SiTypescript, SiPostgresql, SiMysql, SiTailwindcss, SiGithub } from "react-icons/si";
 import { DiGit } from "react-icons/di";
 
@@ -32,19 +33,18 @@ const Tech = () => {
             <hr className="my-2" />
             <ul className=" overflow-y-auto rounded-b-lg text-white" style={{ maxHeight: '220px' }}>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaPython />Python</li>
-              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaHtml5 />HTML</li>
-              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaCss3 />CSS</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaJava />Java</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaJs />JavaScript</li>
-              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><SiTypescript />Typescript</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaReact />React</li>
+              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><SiDjango />Django</li>
+              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaHtml5 />HTML</li>
+              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaCss3 />CSS</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><SiPostgresql />PostgreSQL</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><SiMysql />MySQL</li>
+              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaDocker />Docker</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><SiTailwindcss />Tailwindcss</li>
-              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaNodeJs />NestJS</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><DiGit />Git</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><SiGithub />GitHub</li>
-              <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaDocker />Docker</li>
               <li className="text-xl flex gap-2 items-center p-2 border-b border-zinc-700"><FaWordpress   /> WordPress</li>
               <li className="text-xl flex gap-2 items-center p-2"><FaFigma  /> Figma</li>
             </ul>
